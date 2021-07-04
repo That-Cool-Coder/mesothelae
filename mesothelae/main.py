4bf518624b3b432c06de59876dcb7d11f2f2f615
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-07-04 15:34:39 +0930

    started making socket test

--- mesothelae/main.py
+++ mesothelae/main.py
@@ -1,7 +1,9 @@
 from flask import *
+from flask_socketio import SocketIO
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 from mesothelae import test_routes, error_page_routes
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
--- mesothelae/test_routes.py
+++ mesothelae/test_routes.py
@@ -2,6 +2,7 @@ import random
 
 from flask import *
 from mesothelae.main import app
+from mesothelae.main import socketio
 
 @app.route('/tests/', methods=['GET'])
 def test_server():
@@ -14,3 +15,11 @@ def test_params(param):
 @app.route('/tests/template/', methods=['GET'])
 def test_templates():
     return render_template('test.html', rand_num=random.randint(0, 10))
+
+@app.route('/tests/socket', methods=['GET'])
+def test_socket():
+    return app.send_static_file('socketTest.html')
+
+@socketio.on('json')
+def test_on_json():
+    pass
