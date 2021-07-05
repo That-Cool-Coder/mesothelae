664091e12a52c7d29c13fbbffb7f9e393d834ee0
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-07-05 12:29:53 +0930

    made error codes come from dict

--- mesothelae/error_page_routes.py
+++ mesothelae/error_page_routes.py
@@ -1,22 +1,29 @@
 from flask import *
 from mesothelae.main import app
 
-@app.errorhandler(400)
-def error_400(e):
-    return app.send_static_file('400.html'), 400
 
-@app.errorhandler(401)
-def error_401(e):
-    return app.send_static_file('401.html'), 401
+# A dict of errorcode : information to use for HTTP error pages
+ERROR_PAGE_DATA = {
+    400 : 'Bad request.',
+    401 : 'Access is denied to this page.',
+    403 : 'You are forbidden to view this page.',
+    404 : 'The page you are looking for does not exist.',
+    418 : 'I\'m a teapot!'
+}
 
-@app.errorhandler(403)
-def error_403(e):
-    return app.send_static_file('403.html'), 403
+class ErrorPageInterface:
+    '''This is a simple class designed to allow creating error pages
+    from a dict instead of hard-coding each one in its own function.
+    It acts exactly like a hard-coded function but is configured
+    '''
 
-@app.errorhandler(404)
-def error_404(e):
-    return app.send_static_file('404.html'), 404
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+    
+    def __call__(self, *args, **kwds):
+        return render_template('httpError.html', code=self.code, message=self.message)
 
-@app.errorhandler(418)
-def error_418(e):
-    return app.send_static_file('418.html'), 418
+for error_code in ERROR_PAGE_DATA:
+    interface = ErrorPageInterface(error_code, ERROR_PAGE_DATA[error_code])
+    app.errorhandler(error_code)(interface)
